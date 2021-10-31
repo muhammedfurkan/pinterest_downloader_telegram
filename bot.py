66eaa7fa12d6430652fc0e6d3e4c127db66ebb7a
muhammedfurkan <byazade@yandex.com>
2021-10-31 19:12:26 +0300

    edit

--- bot.py
+++ bot.py
@@ -26,10 +26,12 @@ logger = logging.getLogger(__name__)
 APP_ID = os.environ.get("APP_ID", None)
 APP_HASH = os.environ.get("APP_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
+TMP_DOWNLOAD_DIRECTORY = os.environ.get(
+    "TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 MONGO_DB = os.environ.get("MONGO_DB", None)
 
-bot = TelegramClient("pinterestbot", APP_ID, APP_HASH).start(bot_token=BOT_TOKEN)
+bot = TelegramClient("pinterestbot", APP_ID,
+                     APP_HASH).start(bot_token=BOT_TOKEN)
 
 msg = """
 Merhaba ben Pinterest üzerinden Video ve Resim indirebilen bir botum.
@@ -63,7 +65,8 @@ class pinterest_db:
         elif say > 1:
             cursor = self.collection.find(sorgu, {"_id": 0})
             return {
-                bak["uye_id"]: {"uye_nick": bak["uye_nick"], "uye_adi": bak["uye_adi"]}
+                bak["uye_id"]: {"uye_nick": bak["uye_nick"],
+                                "uye_adi": bak["uye_adi"]}
                 for bak in cursor
             }
         else:
@@ -85,7 +88,8 @@ class pinterest_db:
         if not self.ara({"uye_id": {"$in": [str(uye_id), int(uye_id)]}}):
             return None
 
-        self.collection.delete_one({"uye_id": {"$in": [str(uye_id), int(uye_id)]}})
+        self.collection.delete_one(
+            {"uye_id": {"$in": [str(uye_id), int(uye_id)]}})
         return True
 
     @property
@@ -215,8 +219,8 @@ async def vid(event):
             j = download_video(get_url)
             thumb_image_path = TMP_DOWNLOAD_DIRECTORY + "thumb_image.jpg"
 
-            if not os.path.isdir(thumb_image_path):
-                os.makedirs(thumb_image_path)
+            if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
+                os.makedirs(TMP_DOWNLOAD_DIRECTORY)
 
             metadata = extractMetadata(createParser(j))
             duration = 0
@@ -378,7 +382,8 @@ def time_formatter(seconds: int) -> str:
     result = ""
     v_m = 0
     remainder = seconds
-    r_ange_s = {"days": (24 * 60 * 60), "hours": (60 * 60), "minutes": 60, "seconds": 1}
+    r_ange_s = {"days": (24 * 60 * 60), "hours": (60 * 60),
+                "minutes": 60, "seconds": 1}
     for age, divisor in r_ange_s.items():
         v_m, remainder = divmod(remainder, divisor)
         v_m = int(v_m)
@@ -406,7 +411,8 @@ async def progress(current, total, event, start, type_of_ps):
             round(percentage, 2),
         )
         tmp = progress_str + "{0} of {1}\nETA: {2}".format(
-            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+            humanbytes(current), humanbytes(
+                total), time_formatter(estimated_total_time)
         )
         await event.edit("{}\n {}".format(type_of_ps, tmp))
 
